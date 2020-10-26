ec99425829ba53b2799f344004fa689d40689f45
Ole-Martin Bratteng <ole-martin@bratteng.solutions>
2020-10-26 10:25:03 +0100

    refactor: move colors into theme

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,8 +1,21 @@
-import { colors } from 'styles/colors'
-
 export const theme = {
 	background: '#fff',
 	font: '#000',
 
-	colors,
+	colors: {
+		white: '#fff',
+		black: '#000',
+
+		blue: '#0c3e7e',
+		cyan: '#0f6d9b',
+		purple: '#7b0ea2',
+		magenta: '#d20a5b',
+		red: '#f5390c',
+		yellow: '#fdd835',
+		orange: '#ea8e00',
+		green: '#09b04d',
+		lime: '#75ba2d',
+		brown: '#975000',
+		grey: '#9d9d9d',
+	},
 }
