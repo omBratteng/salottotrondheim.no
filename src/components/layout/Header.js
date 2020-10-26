0004d5ca23107f9134ed6bcf677171d9151e5c25
Ole-Martin Bratteng <ole-martin@bratteng.solutions>
2020-10-26 10:24:46 +0100

    refactor: remove `React` import

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect } from 'react'
+import { useCallback, useEffect } from 'react'
 import styled from 'styled-components'
 import { hideVisually } from 'polished'
 
--- src/components/layout/footer/Maps.js
+++ src/components/layout/footer/Maps.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import styled from 'styled-components'
 import GoogleMapReact from 'google-map-react'
 
