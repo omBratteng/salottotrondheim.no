476813dcaa74f19696163aca9dce5a57af370448
Ole-Martin Bratteng <ole-martin@bratteng.solutions>
2020-10-26 01:20:21 +0100

    Add more favicons

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -32,7 +32,32 @@ const App = ({ Component, pageProps }) => {
 				/>
 				<meta httpEquiv="X-UA-Compatible" content="ie=edge" />
 				<meta name="apple-mobile-web-app-capable" content="yes" />
-				<link rel="icon" type="image/png" href="/favicon.png" />
+
+				<link
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href="/apple-touch-icon.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/favicon-32x32.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="/favicon-16x16.png"
+				/>
+				<link rel="manifest" href="/site.webmanifest" />
+				<link
+					rel="mask-icon"
+					href="/safari-pinned-tab.svg"
+					color="#231f20"
+				/>
+				<meta name="msapplication-TileColor" content="#231f20" />
+				<meta name="theme-color" content="#231f20" />
 
 				<meta property="og:title" content="Salotto" />
 				<meta
