6a07f2786ae01d9cec5454225db92cffe8a40784
Ole-Martin Bratteng <ole-martin@bratteng.solutions>
2020-10-28 16:01:28 +0100

    refactor: create new Section component to have a uniform style

---
+++ src/components/layout/section/Section.js
@@ -0,0 +1,29 @@
+import styled, { css } from 'styled-components'
+import PropTypes from 'prop-types'
+
+const willBleed = (type) => type === 'wide' || type === 'pseudo'
+
+const Section = styled.section`
+	grid-column: ${({ type }) => (willBleed(type) ? '1 / -1' : '2')};
+	margin: 0 auto;
+	padding: ${({ type }) => (willBleed(type) ? '0' : '0 1.5rem')};
+	width: 100%;
+
+	${({ type }) =>
+		type === 'pseudo' &&
+		css`
+			max-width: 1600px;
+		`}
+
+	${({ type }) =>
+		type === 'narrow' &&
+		css`
+			max-width: 73ch;
+		`}
+`
+
+Section.propTypes = {
+	type: PropTypes.oneOf(['pseudo', 'wide', 'narrow']),
+}
+
+export default Section
