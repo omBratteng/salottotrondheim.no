5c5eea6862c01b6bc208ee9012364eb615a1d768
Ole-Martin Bratteng <ole-martin@bratteng.solutions>
2020-11-23 21:21:10 +0100

    Move assets from `src/assets` to `public/assets`

--- src/assets/products/index.js
+++
@@ -1,7 +0,0 @@
-import Evo from './evo-logo.svg'
-import Ghd from './ghd-logo.svg'
-import Tigi from './tigi-logo.svg'
-import Gold from './gold-logo.svg'
-import Olaplex from './olaplex-logo.svg'
-
-export { Evo, Ghd, Tigi, Gold, Olaplex }
