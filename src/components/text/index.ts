2927ecbddf365961024a9ffc9ec5284d2a22f33e
Ole-Martin Bratteng <ole-martin@bratteng.solutions>
2020-11-30 15:31:56 +0100

    don't export h4-h6

--- src/components/text/index.ts
+++ src/components/text/index.ts
@@ -1,9 +1,6 @@
 import H1 from './H1'
 import H2 from './H2'
 import H3 from './H3'
-const H4 = false
-const H5 = false
-const H6 = false
 import P from './P'
 
-export { H1, H2, H3, H4, H5, H6, P }
+export { H1, H2, H3, P }
