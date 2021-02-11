afcc74ced2ec1f6b50d722465890ac0d2838c67f
Ole-Martin Bratteng <ole-martin@bratteng.solutions>
2021-02-11 11:51:20 +0100

    fix: replace `js-yaml` with `yaml`

--- src/utils/getPricelist.ts
+++ src/utils/getPricelist.ts
@@ -1,26 +1,17 @@
 import fs from 'fs'
 import { join } from 'path'
 import process from 'process'
-import yaml from 'js-yaml'
+import yaml from 'yaml'
 
 const getFile = (): string =>
 	fs.readFileSync(join(process.cwd(), 'public', 'prices.yaml'), 'utf-8')
 
-export type PriceListInterface = (
-	| {
-			name: null
-			prices: {
-				name: string
-				price: number[]
-			}[]
-	  }
-	| {
-			name: string
-			prices: {
-				name: string
-				price: number[]
-			}[]
-	  }
-)[]
+export type PriceListInterface = {
+	name: null | string
+	prices: {
+		name: string
+		price: number[]
+	}[]
+}[]
 
-export const getPricelist = (): PriceListInterface => yaml.load(getFile())
+export const getPricelist = (): PriceListInterface => yaml.parse(getFile())
