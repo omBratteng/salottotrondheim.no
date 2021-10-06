26474620ce7b3c42be07bf20f4f4fa5c1d795014
Ole-Martin Bratteng <ole-martin@bratteng.com>
2021-10-07 00:35:06 +0200

    chore: enable compression of imgix images

--- src/utils/imgixLoader.ts
+++ src/utils/imgixLoader.ts
@@ -3,6 +3,6 @@ import { ImageLoaderProps } from 'next/image'
 import { IMGIX_URL } from 'site-config'
 
 const imgixLoader = ({ src, width, quality }: ImageLoaderProps): string =>
-	`${IMGIX_URL}${src}?auto=format&w=${width}&q=${quality || 75}`
+	`${IMGIX_URL}${src}?auto=format,compress&w=${width}&q=${quality || 75}`
 
 export default imgixLoader
