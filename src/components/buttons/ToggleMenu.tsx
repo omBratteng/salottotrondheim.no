8d49a62b0dcb9cd19a8f7c75f5de767d41f53e84
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2022-05-22 07:42:02 +0200

    fix: emotion SSR pseudo types

--- src/components/buttons/ToggleMenu.tsx
+++ src/components/buttons/ToggleMenu.tsx
@@ -19,7 +19,7 @@ const Bars = styled.span`
 			width 0.2s ease 0.2s;
 	}
 
-	> span:nth-child(1) {
+	> span:nth-of-type(1) {
 		left: 0;
 		margin: -2px 0 0 0;
 		top: 50%;
@@ -27,14 +27,14 @@ const Bars = styled.span`
 		width: 40px;
 	}
 
-	> span:nth-child(2) {
+	> span:nth-of-type(2) {
 		left: 0;
 		top: 2px;
 		transform-origin: 0 50%;
 		width: 20px;
 	}
 
-	> span:nth-child(3) {
+	> span:nth-of-type(3) {
 		bottom: 2px;
 		right: 0;
 		transform: translate(0, 0);
@@ -57,8 +57,8 @@ const Button = styled.button`
 	z-index: 100;
 
 	&[aria-expanded='false']:hover ${Bars} {
-		> span:nth-child(2),
-		> span:nth-child(3) {
+		> span:nth-of-type(2),
+		> span:nth-of-type(3) {
 			width: 40px;
 		}
 	}
@@ -70,23 +70,23 @@ const Button = styled.button`
 			background: ${(props) => props.theme.colors.white};
 		}
 
-		> span:nth-child(1),
-		> span:nth-child(2),
-		> span:nth-child(3) {
+		> span:nth-of-type(1),
+		> span:nth-of-type(2),
+		> span:nth-of-type(3) {
 			transition: transform 0.45s cubic-bezier(0.9, -0.6, 0.3, 1.6) 0.1s, width 0.2s ease;
 			width: 19px;
 		}
 
-		> span:nth-child(1) {
+		> span:nth-of-type(1) {
 			transform: rotate(-45deg);
 			width: 40px;
 		}
 
-		> span:nth-child(2) {
+		> span:nth-of-type(2) {
 			transform: translate(6px, 0) rotate(45deg);
 		}
 
-		> span:nth-child(3) {
+		> span:nth-of-type(3) {
 			transform: translate(-6px, 0) rotate(45deg);
 		}
 	}
