728f8c7c58258a6763750022574af4f56ffaf2c5
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2023-01-28 00:29:24 +0100

    Cleanup deprecated code

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -4,8 +4,6 @@ import styled from '@emotion/styled'
 import Header from 'components/layout/Header'
 import Footer from 'components/layout/Footer'
 
-import useAnalytics from 'hooks/useAnalytics'
-
 const Main = styled.main`
 	display: grid;
 	flex: 1;
@@ -18,15 +16,6 @@ type Props = {
 }
 
 const Layout = ({ children }: Props): JSX.Element => {
-	useAnalytics({
-		domainId: 'f374ffdb-fc4c-497c-9a28-50ae5c9b769e',
-		server: 'https://analytics.bratteng.cloud',
-		options: {
-			detailed: true,
-			ignoreLocalhost: true,
-		},
-	})
-
 	return (
 		<>
 			<Header />
