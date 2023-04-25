283cfe3160590878bdf860070c8f0afeb886a6b1
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2023-04-25 20:38:58 +0200

    Rewrite loader

--- src/utils/imgixLoader.ts
+++ src/utils/imgixLoader.ts
@@ -1,8 +1,17 @@
-import { ImageLoaderProps } from 'next/legacy/image'
+import { ImageLoaderProps } from 'next/image'
 
 import { IMGIX_URL } from 'site-config'
 
-const imgixLoader = ({ src, width, quality }: ImageLoaderProps): string =>
-	`${IMGIX_URL}${src}?auto=format,compress&w=${width}&q=${quality || 75}`
+const imgixLoader = ({ src, width, quality }: ImageLoaderProps): string => {
+	const url = new URL(`${IMGIX_URL}${src}`)
+	const params = url.searchParams
+
+	params.set('auto', params.getAll('auto').join(',') || 'format,compress')
+	params.set('fit', params.get('fit') || 'max')
+	params.set('w', params.get('w') || width.toString())
+	params.set('q', quality?.toString() || '75')
+
+	return url.href
+}
 
 export default imgixLoader
