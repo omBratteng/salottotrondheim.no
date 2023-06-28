636ed2b9a06a4a1835ce9eac1cbd1c91902f6d92
Ole-Martin Bratteng <1681525+omBratteng@users.noreply.github.com>
2023-06-28 15:37:35 +0200

    Fix Park logo size

--- src/products/index.ts
+++ src/products/index.ts
@@ -1,8 +1,9 @@
 import Evo from './evo-logo.svg'
 import Ghd from './ghd-logo.svg'
-import Tigi from './tigi-logo.svg'
-import PhilipMartin from './philip-martin-s.svg'
 import Gold from './gold-logo.svg'
 import Olaplex from './olaplex-logo.svg'
+import Park from './park-logo.svg'
+import PhilipMartin from './philip-martin-s.svg'
+import Tigi from './tigi-logo.svg'
 
-export { Evo, Ghd, Tigi, Gold, Olaplex, PhilipMartin }
+export { Evo, Ghd, Tigi, Gold, Olaplex, Park, PhilipMartin }
