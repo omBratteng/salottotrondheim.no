131294780af2b578fc60de8cd93017d3c4edcd36
Matteo <matteodicapua@users.noreply.github.com>
2025-08-17 16:34:23 +0200

    Add Nina to employees and update text on everyone

--- src/components/employees/list.tsx
+++ src/components/employees/list.tsx
@@ -7,10 +7,9 @@ const list = [
 		description: (
 			<>
 				<P>
-					Pål er frisør, stylist, og kreativ ansvarlig hos Salotto med mer enn 20 års erfaring i faget. Som frisør er
-					Pål en uformell spøkefugl med teknisk finesse og et lyttende øre.
+					Pål er frisør, stylist, og kreativ ansvarlig hos Salotto med 30 års erfaring i faget. Som frisør er Pål en uformell spøkefugl som elsker å tulle å ha det gøy på jobb med kunder og ansatte. Med sin kreative hjerne og tekniske finesse, finner Pål den perfekte frisyren til deg.  
 				</P>
-				<P>Trenger du en ny frisyre, få høre den usminkede sannhet eller bare å skrifte, så er Pål din mann.</P>
+				<P>Ønsker du å fikse håret samtidig som du får litt sladder og en god latter, da er Pål frisøren for deg.</P>
 			</>
 		),
 	},
@@ -20,8 +19,7 @@ const list = [
 		description: (
 			<>
 				<P>
-					Lindis er frisør, stylist og daglig leder av Salotto og har mer enn 30 års erfaring. Med høy kompetanse
-					elsker Lindis faget sitt og gleder seg hver dag til å gi en god opplevelse med best mulig resultat.
+					Lindis er frisør, stylist og daglig leder av Salotto og har 40 års erfaring i faget. Med mange år med reising og kurs holding for å lære bort sin kompetanse og kunnskap, elsker Lindis faget sitt og gleder seg hver dag til å finne den beste fargen og frisyren for ditt ansikt.
 					<br />
 					Vil du ha hjelp med frisyren i hyggelig selskap og omgivelser, så er Lindis din frisør.
 				</P>
@@ -34,12 +32,9 @@ const list = [
 		description: (
 			<>
 				<P>
-					Kristine er frisør og har snart 10-års erfaring i faget.
-					<br />
-					Hun elsker å gi kundene en god opplevelse og å være kreativ.
-					<br />
-					Enten om du ønsker å slå av en prat eller bare sitte å nyte stillheten samtidig som du blir ny på håret så
-					er Kristine frisøren for deg!
+					Kristine er frisør og har over 10-års erfaring i faget. Hun er veldig detaljert og resultat orientert. Hun er eksepsjonell til blondiner samt også diverse kreative frisyrer.
+Med en sprudlende, kreativ sjel og en latter som smitter. Er Kristine en frisør som liker å bli godt kjent med sine kunder.
+Enten om du ønsker å slå av en prat eller bare sitte å nyte stillheten samtidig som du blir ny på håret, så er Kristine frisøren for deg.
 				</P>
 			</>
 		),
@@ -50,9 +45,23 @@ const list = [
 		description: (
 			<>
 				<P>
-					Etter to år på frisørskole og to år som lærling hos oss, har Solvår fullført utdanningen sin med godkjent
-					svennebrev. Hun tok andreplass i skole NM i Yrkesfag (frisering). Hun er en veldig dyktig, rolig og
-					behagelig frisør med et øye for detaljer.
+					Solvår har snart 10 års erfaring i frisør faget. 
+Hun tok sølvmedalje i skole NM i Yrkesfag (frisering). 
+Hun er en rolig og behagelige person, som er opptatt av å gi kundene en frisør-opplevelse som lever opp til slagordet vårt. Hun er kreativ og detalj fokusert og elsker å finne den perfekte frisyren til en hver person. 
+Ønsker du en spa dag for hodet eller en frisør som ikke er redd for å bryne seg på en kreativ klipp, så er Solvår frisøren for deg.
+				</P>
+			</>
+		),
+	},
+	{
+		name: 'Nina',
+		image: 'Nina.jpg',
+		description: (
+			<>
+				<P>
+					Nina har over 30 års erfaring som frisør. Hun er opptatt av å se hele mennesket og brenner for å skape en rolig og trygg atmosfære for kundene sine. Enten det er en klipp, farge eller en god samtale som står på timeplanen.
+Nina kombinerer sin solide faglige kompetanse med en holistisk tilnærming, og for henne handler frisørfaget ikke bare om hår, men også om velvære og selvfølelse. 
+Ønsker du en varm frisør med god intuisjon og evne til å få deg til å føle deg sett og ivaretatt, da er Nina frisøren til deg.
 				</P>
 			</>
 		),
